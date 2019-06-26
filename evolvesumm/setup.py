bcd179ddcbf730eb1f86031827790193cb7882d1
Matt Eding <matteding@gmail.com>
2019-06-26 16:40:07 -0700

    cython util funcs working with 4x speedup vs numba in pipeline

--- evolvesumm/setup.py
+++ evolvesumm/setup.py
@@ -3,9 +3,9 @@ from Cython.Build import cythonize
 import numpy
 
 
-ext = Extension("inter",
+ext = Extension("util",
                 sources=["utils.pyx"])
 
-setup(name="inter",
+setup(name="util",
       ext_modules=cythonize(ext),
       include_dirs=[numpy.get_include()])
--- evolvesumm/utils.pyx
+++ evolvesumm/utils.pyx
@@ -1,12 +1,10 @@
 import numpy as np
 
+cimport numpy as cnp
 from cython cimport boundscheck, wraparound
 from cython.parallel cimport prange
-cimport numpy as cnp
-
 from libc.math cimport exp
 
-cimport libc.math as cmath
 
 
 @boundscheck(False)
@@ -21,49 +19,23 @@ cdef double _jaccard_similarity(int[::1] a, int[::1] b) nogil:
 
     cdef int i, n = len(a), union_ = 0, intersection = 0
     cdef double ratio
-    for i in prange(n, nogil=True):
+
+    for i in range(n):
         union_ += a[i] | b[i]
         intersection += a[i] & b[i]
     ratio = intersection / <double>union_
     return ratio
 
 
-# faster than pyobj but slower than numba
-def jaccard_similarity_cython(a, b):
-    return _jaccard_similarity(a, b)
-
-
-def jaccard_similarity_pyobj(cnp.ndarray a, cnp.ndarray b):
-    """
-    Return the ratio of the intersection to the union of two containers.
-
-    | a ∩ b |
-    —————————
-    | a ∪ b |
-    """
-    cdef cnp.ndarray intersection = a & b, union_ = a | b
-    cdef double ratio = intersection.sum() / union_.sum()
-    return ratio
-
-
-def sigmoid_pyobj(double x):
-    """Sigmoid function defined as 1 / (1 + exp(-x))."""
-    return 1 / (1 + exp(-x))
-
-
 cdef double _sigmoid(double x):
     return 1 / (1 + exp(-x))
-
-# same as pyobj speed
-def sigmoid_cython(x):
-    return _sigmoid(x)
 
 
 @boundscheck(False)
 @wraparound(False)
 def cohesion(cnp.ndarray chrom, cnp.ndarray doc):
     """Measure of how compact all the clusters are.
-    
+
      k            sim(Si, Sj)
      ∑      ∑     ———————————
     p=1  Si,Sj∈Cp    |Cp|
@@ -84,7 +56,6 @@ def cohesion(cnp.ndarray chrom, cnp.ndarray doc):
     return total
 
 
-
 @boundscheck(False)
 @wraparound(False)
 def separation(cnp.ndarray chrom, cnp.ndarray doc):
@@ -97,7 +68,7 @@ def separation(cnp.ndarray chrom, cnp.ndarray doc):
 
     cdef:
         double total = 0
-        int i, j, k, m, n, p, q = len(np.unique(chrom))
+        int i, j, k = len(np.unique(chrom)), m, n, p, q = len(np.unique(chrom))
         int[:, ::1] sents_p, sents_q
     
     #: itertools.combinations(k, r=2)
@@ -111,3 +82,65 @@ def separation(cnp.ndarray chrom, cnp.ndarray doc):
                 for j in range(n):
                     total += _jaccard_similarity(sents_p[i], sents_q[j]) / m / n
     return total
+
+
+def cohesion_separation(cnp.ndarray chrom, cnp.ndarray doc):
+    """Measure balancing both cohesion and separation of clusters."""
+    cdef double coh, sep
+    coh = cohesion(chrom, doc)
+    sep = separation(chrom, doc)
+    return (1 + _sigmoid(coh)) ** sep
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# For quick testing & benchmarking in ipython
+#
+# NOTES: for some reason casting as int32 (int) is 4x faster than uint8 (unsigned char);
+#           2x faster than int8 (char);
+#           no np.bool_ memview support;
+#           at least the int version is 4x faster than my numba version! :D
+#           have to manually cast doc from bool to int32
+#
+# GOAL: parallelize the multiprocessing 
+#           use pointers on flatten versions of array and keep track of row lengths
+#           for doc[chrom == k] can loop thru memoryview once and store clusters in mapping
+#               is there a defaultdict(list) for c/c++?
+#               want to store as map[cluster].append(sentence index)
+#               **ptr to do the trick?
+
+from nltk.tokenize import sent_tokenize
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+import bitarray
+
+
+def get_document(text):
+    stop_words = None
+    text = str(text)
+    tokens = sent_tokenize(text.lower())
+    count_vec = CountVectorizer(stop_words=stop_words).fit_transform(tokens)
+    #: numba does not support sparse matrices; dtype bool to emulate sets
+    document = count_vec.toarray().astype(bool)
+    return document
+
+
+def get_chromosome(document):
+    summ_len = 5
+    #: make a chromosome that is a random partition with each cluster.
+    clusters = np.arange(summ_len)
+    chrom = np.full(len(document), -1)
+    #: ensure that each cluster is accounted for at least once
+    idxs = np.random.choice(np.arange(len(chrom)), summ_len, replace=False)
+    chrom[idxs] = np.random.permutation(clusters)
+    #: fill rest randomly
+    idxs = (chrom == -1)
+    chrom[idxs] = np.random.choice(clusters, np.sum(idxs))
+    return chrom
+
+
+with open('/Users/matteding/Desktop/Metis/EvolveSumm/poe.txt') as fp:
+    text = fp.read()
+
+np.random.seed(0)
+doc = get_document(text).astype(np.int32)
+chrom = get_chromosome(doc)
